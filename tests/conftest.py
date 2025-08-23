91698df8361472a6898a4353930d1db6a08ad11c
jokelbaf <60827680+JokelBaf@users.noreply.github.com>
2025-08-23 04:53:16 +0300

    test: Support proxy in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import os
+
+import aiohttp
+import dotenv
 import pytest
 
 import hhaven
@@ -6,5 +10,23 @@ import hhaven
 @pytest.fixture(scope="session")
 async def client():
     """Return a HHaven client instance."""
-    client = await hhaven.Client(debug=True).build()
+    proxy, proxy_auth = get_proxy_config()
+    client = await hhaven.Client(debug=True, proxy=proxy, proxy_auth=proxy_auth).build()
     return client
+
+def pytest_configure(config: pytest.Config) -> None:
+    """Configure pytest."""
+    dotenv.load_dotenv()
+
+def get_proxy_config() -> tuple[str | None, aiohttp.BasicAuth | None]:
+    """Get proxy configuration from environment variables."""
+    proxy = os.getenv("PROXY_URL")
+    username = os.getenv("PROXY_USERNAME")
+    password = os.getenv("PROXY_PASSWORD")
+
+    proxy_auth = aiohttp.BasicAuth(
+        login=username,
+        password=password,
+    ) if username and password else None
+
+    return proxy, proxy_auth
